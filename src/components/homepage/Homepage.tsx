5b6a3195b44fac398795c3e4bbd9c7fd78181d94
mag-e-williams <margretwilliams42@gmail.com>
2023-04-07 16:29:12 -0400

    initial

---
+++ src/components/homepage/Homepage.tsx
@@ -0,0 +1,59 @@
+import { useData } from 'api/useData';
+import { ContentGrid } from 'components/ContentGrid';
+// import { MapPreviewCard } from 'components/homepage/MapPreviewCard';
+import { HOMEPAGE_TITLE, Meta } from 'components/Meta';
+import { useGridAnimation } from 'hooks/useGridAnimation';
+import { useMemo, useRef } from 'react';
+// import { IntroCard } from './IntroCard';
+// import { ProjectCard } from './ProjectCard';
+// import { SpotifyCard } from './SpotifyCard';
+// import { StravaCard } from './StravaCard';
+
+/**
+ * Puts all projects into a grid using `projects` data,
+ * interspersed with `introBlock` data, and dark/light mode
+ * toggle.
+ */
+export function Homepage() {
+  // const { data: projects } = useData('projects');
+  // const { data: introBlock } = useData('intro');
+
+  // Grabs the first intro block text element, essentially.
+  const firstParagraph = '';
+  // introBlock?.textBlock?.content?.json.content
+  //   ?.find((item) => item.nodeType === 'paragraph')
+  //   ?.content?.find((item) => item.nodeType === 'text')?.value;
+
+  // For animating grid items
+  const gridRef = useRef<HTMLDivElement | null>(null);
+  const turnOnAnimation = useGridAnimation(gridRef);
+
+  const projectCards = null;
+    // projects?.map((project) => (
+    //   <ProjectCard key={project.title} {...project} turnOnAnimation={turnOnAnimation} />
+    // )) ?? [];
+
+  // These index into projectCards to splice in other cards
+  const otherCards = null;
+  // useMemo(
+  //   () => [
+  //     // { index: 0, card: <IntroCard key="intro" /> },
+  //     // { index: 0, card: <MapPreviewCard key="map" turnOnAnimation={turnOnAnimation} /> },
+  //     { index: 2, card: <SpotifyCard key="spotify" /> },
+  //     // { index: 6, card: <StravaCard key="strava" /> },
+  //   ],
+  //   [turnOnAnimation],
+  // );
+
+  return (
+    <>
+      <Meta title={HOMEPAGE_TITLE} description={firstParagraph} />
+      <ContentGrid gridRef={gridRef}>
+        {/* {otherCards.map(({ index, card }, arrayIndex) => {
+          const nextItem = otherCards[arrayIndex + 1];
+          return [card, ...projectCards.slice(index, nextItem?.index)];
+        })} */}
+      </ContentGrid>
+    </>
+  );
+}
